a749e24f63dbcd34541f93817885c6698de326c3
Mahatma Arrayyan <xxryaan3010@gmail.com>
2024-04-30 11:30:17 +0800

    form done

--- reservasi.php
+++ reservasi.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" href="assets/css/style.css">
 
     <!-- Google Fonts -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="ht)tps://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 
@@ -18,6 +18,7 @@
     <title>Historia Meze Grill</title>
 </head>
 <body>
+    
     <header class="navigation d-flex align-items-center justify-content-between container">
         <h2 class="fw-semibold">
             Historia Meze Grill<span class="text-danger">.</span>
@@ -81,48 +82,63 @@
                         </div>
                     </div>
                     <div class="mb-3 row">
-                        <label for="paket" class="col-sm-3 col-form-label">Pilih Makanan</label>
+                        <label for="paket" class="col-sm-3 col-form-label">Pilih Lauk</label>
                         <div class="col-sm-9 row">
-                            <div class="col-6">
+                            <div class="col-4">
                                 <div class="form-check pe-3">
-                                    <input class="form-check-input" type="radio" name="sate" id="sate" value="sate">
+                                    <input class="form-check-input" type="checkbox" name="sate" id="sate" value="sate">
                                     <label class="form-check-label" for="sate">
-                                        Sate Daging (20k)
+                                        Sate (10k)
                                     </label>
                                 </div>
                                 <div class="form-check pe-3">
-                                    <input class="form-check-input" type="radio" name="nasgor" id="nasgor" value="nasgor">
-                                    <label class="form-check-label " for="nasgor">
-                                        Nasi Goreng (25k)
+                                    <input class="form-check-input" type="checkbox" name="rendang" id="rendang" value="rendang">
+                                    <label class="form-check-label " for="rendang">
+                                        Rendang (15k)
                                     </label>
                                 </div>
                             </div>
-                            <div class="col-6">
+                            <div class="col-4">
                                 <div class="form-check pe-3">
-                                    <input class="form-check-input" type="radio" name="steak" id="steak" value="steak">
-                                    <label class="form-check-label " for="steak">
-                                        Daging Steak (50k)
+                                    <input class="form-check-input" type="checkbox" name="sayur" id="sayur" value="sayur">
+                                    <label class="form-check-label " for="sayur">
+                                        Sayur (5k)
                                     </label>
                                 </div>
                                 <div class="form-check pe-3">
-                                    <input class="form-check-input" type="radio" name="ayamBakar" id="ayamBakar" value="ayamBakar">
-                                    <label class="form-check-label " for="ayamBakar">
-                                        Ayam Bakar (30k)
+                                    <input class="form-check-input" type="checkbox" name="ayam" id="ayam" value="ayam">
+                                    <label class="form-check-label " for="ayam">
+                                        Ayam (12k)
+                                    </label>
+                                </div>
+                            </div>
+                            <div class="col-4">
+                                <div class="form-check pe-3">
+                                    <input class="form-check-input" type="checkbox" name="sosis" id="sosis" value="sosis">
+                                    <label class="form-check-label " for="sosis">
+                                        Sosis (8k)
+                                    </label>
+                                </div>
+                                <div class="form-check pe-3">
+                                    <input class="form-check-input" type="checkbox" name="mie" id="mie" value="mie">
+                                    <label class="form-check-label " for="mie">
+                                        Mie (10k)
                                     </label>
                                 </div>
                             </div>
                         </div>
                     </div>
+
                     <div class="mb-3 row">
                         <label for="nama" class="col-sm-3 col-form-label">Harga Paket</label>
                         <div class="col-sm-9">
-                            <input type="name" class="form-control" id="namaReservasi" value="java">
+                            <input type="name" class="form-control" id="hargaPaket">
                         </div>
                     </div>
                     <div class="mb-3 row">
                         <label for="nama" class="col-sm-3 col-form-label">Total Tagihan</label>
                         <div class="col-sm-9">
-                            <input type="name" class="form-control" id="namaReservasi">
+                            <input type="name" class="form-control" id="totalTagihan">
                         </div>
                     </div>
                     <div class="btn-reservasi d-flex justify-content-end">
@@ -138,38 +154,35 @@
         
     </footer>
     <!-- Bootstrap -->
-    <script src="assets/js/main.js">
-        jumKursi = document.getElementById('kursi').value;
-        hitung = document.getElementById('hitung');
-        
-        totalTagihan = 0;
+    <script>
+        function hitungTotal() {
+            let jumKursi = parseInt(document.getElementById('kursi').value);
+            let paket = 0;
+            if(document.getElementById('reguler').checked){
+                paket = 1000000;
+            }
+            else if(document.getElementById('ulangTahun').checked){
+                paket = 1500000;
+            }
+            else if(document.getElementById('ruangMeeting').checked){
+                paket = 2000000;
+            }
 
-        if(document.getElementById('reguler')){
-            totalTagihan+=1000000;
-        }
-        if(document.getElementById('ulangTahun')){
-            totalTagihan+=1500000;
-        }
-        if(document.getElementById('ruangMeeting')){
-            totalTagihan+=2000000;
-        }
-        if(document.getElementById('sate')){
-            totalTagihan+=20000;
-        }
-        if(document.getElementById('nasiGoreng')){
-            totalTagihan+=25000;
-        }
-        if(document.getElementById('steak')){
-            totalTagihan+=50000;
-        }
-        if(document.getElementById('ayamBakar')){
-            totalTagihan+=30000;
-        }
+            let sate = document.getElementById('sate').checked ? 10000 : 0;
+            let rendang = document.getElementById('rendang').checked ? 15000 : 0;
+            let sayur = document.getElementById('sayur').checked ? 5000 : 0;
+            let ayam = document.getElementById('ayam').checked ? 12000 : 0;
+            let sosis = document.getElementById('sosis').checked ? 8000 : 0;
+            let mie = document.getElementById('mie').checked ? 12000 : 0;
 
-        function hitungTotal(jumKursi, paket, makanan){
-            alert(jumKursi * makanan + paket);
+            let makananTotal = sate + rendang + sayur + ayam + sosis + mie;
+            let totalTagihan = jumKursi * makananTotal + paket;
+
+            document.getElementById('hargaPaket').value = paket;
+            document.getElementById('totalTagihan').value = totalTagihan;
         }
     </script>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
